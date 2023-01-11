3609809a55a00989b9f65e3f74ef8ef2038215e3
Vaibhav Dwivedi <98420486+vaibhavdwivedi2710@users.noreply.github.com>
2023-01-11 21:56:02 +0530

    Added solution - LeetHub

---
+++ Print the pattern | Set-1 - GFG/print-the-pattern-set1.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+void printPat(int n);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n;
+	cin>>n;
+    printPat(n);
+    cout<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+void printPat(int n)
+{
+ int i,j,k;
+ for(k=0;k<n;k++){
+     for(i=n;i>0;i--){
+         for(j=0;j<n-k;j++){
+             cout<<i<<" ";
+         }
+     }
+     cout<<"$";
+ }
+}
