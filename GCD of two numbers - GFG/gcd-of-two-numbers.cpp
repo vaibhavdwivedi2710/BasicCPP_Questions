ba614093e978b8c5137c6fd92399fadfa8307297
Vaibhav Dwivedi <98420486+vaibhavdwivedi2710@users.noreply.github.com>
2023-01-13 01:55:58 +0530

    Added solution - LeetHub

--- GCD of two numbers - GFG/gcd-of-two-numbers.cpp
+++ GCD of two numbers - GFG/gcd-of-two-numbers.cpp
@@ -9,27 +9,9 @@ class Solution
 	public:
     int gcd(int A, int B) 
 	{ 
-	  if(A == 0){
-            return B;
-        }
-        
-        if(B == 0){
-            return A;
-        }
-        
-        if(A == B){
-            return A;
-        }
-        
-        if(A > B){
-            return gcd(B, A % B);
-        }
-        
-        if(B > A){
-            return gcd(B % A, A);
-        }
-        
-        return -1;
+	  if(B==0)
+	  return A;
+	  return gcd(B,A%B);
 	      
 	} 
 };
