67b92b4dbb25f99b2ab20d46d884c2bbeb25fa03
Vaibhav Dwivedi <98420486+vaibhavdwivedi2710@users.noreply.github.com>
2023-01-25 09:58:18 +0530

    Added solution - LeetHub

---
+++ Smallest window in a string containing all the characters of another string - GFG/smallest-window-in-a-string-containing-all-the-characters-of-another-string.cpp
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the smallest window in the string s consisting
+    //of all the characters of string p.
+    string smallestWindow (string s, string p)
+    {
+       int count = 0;
+
+        int mp[256] = {0};
+
+        
+
+        for(int i = 0; i<p.length();i++)
+
+        {
+
+            if (mp[p[i]] == 0) count += 1;
+
+            mp[p[i]]++;
+
+        }
+
+        
+
+        int start = 0;
+
+        int res = INT_MAX;
+
+        int i = 0;
+
+        int j = 0;
+
+        
+
+        while (j<s.length())
+
+        {
+
+            mp[s[j]]--;
+
+            
+
+            if (mp[s[j]] == 0) count--;
+
+            
+
+            if (count == 0)
+
+            {
+
+                while (count == 0)
+
+                {
+
+                    if (j-i+1 < res)
+
+                    {
+
+                        res = j-i+1;
+
+                        start = i;
+
+                    }
+
+                    mp[s[i]]++;
+
+                    if (mp[s[i]] == 1) count += 1;
+
+                    i += 1;
+
+                }
+
+            }
+
+            j += 1;
+
+        }
+
+        if (res != INT_MAX) return s.substr(start,res);
+
+        return "-1";
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        string pat;
+        cin>>pat;
+        Solution obj;
+        cout<<obj.smallestWindow(s, pat)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
